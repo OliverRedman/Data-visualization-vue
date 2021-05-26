87632972ad41825f2c902ad0b4a1123490965d88
OliverRedman <oliredman@hotmail.com>
2021-05-26 23:51:16 +0100

    candle + linechart

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,47 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div class="data">
+    <apexchart width="500" type="line" :options="options" :series="series"></apexchart>
+        <apexchart width="600" type="candlestick" :options="candleSeries[0].chartOptions" :series="candleSeries[0].series"></apexchart>
+
+
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      options: {
+        chart: {
+          id: 'vuechart-example'
+        },
+        xaxis: {
+          categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998]
+        }
+      },
+      series: [{
+        name: 'series-1',
+        data: [30, 40, 45, 50, 49, 60, 70, 91]
+      }]
+    }
+    
+  },
+ 
+computed: {
+  candleSeries() {
+    return this.$store.state.candlestick
   }
+},
+mounted() {
+console.log(this.candleSeries)
+}
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.data {
+  display: flex;
+  flex-wrap: wrap;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import store from './store'
+import VueApexCharts from "vue3-apexcharts";
 
-createApp(App).use(store).mount('#app')
+const app = createApp(App)
+app.use(VueApexCharts);
+
+
+app.use(store)
+
+app.mount('#app')
